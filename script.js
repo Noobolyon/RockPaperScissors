da6d99ed2aeab554b66369b00e1c642bcd08fc06
Youssef <youssefelsaid678@gmail.com>
2023-01-26 09:39:59 +0200

    Added UI and functionality to the game.

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
-
+const playerScore=document.querySelector("#h");
+const computerScore=document.querySelector("#c");
+let pScore=0;
+let cScore=0;
+const result = document.querySelector("#result");
+result.textContent = "";
 
 
 function getComputerChoice(){
-    let choices = ["rock","paper","scissors"];
+    const choices = ["rock","paper","scissors"];
     //this retruns choices[from 0 to array length]
     return choices[Math.floor(Math.random()*choices.length)];  
 }
+const btns = document.querySelectorAll("button");
+btns.forEach(btn => btn.addEventListener("click",playRound));
+
+function updateScore(p,c){
+    playerScore.textContent=p;
+    computerScore.textContent=c;
+}
 
 
-let playerSelection = prompt("enter your choice: ");
-
-function playRound(playerSelection,computerSelection=getComputerChoice()){
+function playRound(){
     
-    let p = playerSelection.toLowerCase();
-    let c = computerSelection;
+    let p = this.id;
+    let c = getComputerChoice();
+
     if((p=="rock"&&c=="scissors")||(p=="paper"&&c=="rock")||(p=="scissors"&&c=="paper")){
-        return `You Win! ${p} beats ${c}`;
+        result.textContent = (`You Win! ${p} beats ${c}`);
+        pScore++;
+
     }
     else if((c=="rock"&&p=="scissors")||(c=="paper"&&p=="rock")||(c=="scissors"&&p=="paper")){
-        return `You Lose! ${p} is beaten by ${c}`;
+        result.textContent = (`You Lose! ${p} is beaten by ${c}`); 
+        cScore++;
     }
     else if(c===p){
-        return "It's a Tie!";
+        result.textContent = ("It's a Tie!"); 
     }
     else{
-        return "error";
+        result.textContent = (playerSelection); 
     }
-
+    updateScore(pScore,cScore);
+    winner();
+    
+}
+function winner(){
+    if(pScore==5){
+        result.textContent="YOU WIN !!";
+        pScore==0,cScore==0;
+        updateScore(0,0)
+    }
+    else if(cScore==5){
+        result.textContent="YOU LOSE :(";
+        pScore==0,cScore==0;
+        updateScore(0,0);
+    }
+    else
+        return;
 }
 
 
-function game(){
-    for(let i = 0;i<5;i++){
-        console.log(playRound(playerSelection));
-    }
-}
+
+
+
+
+
+
+
 
 
